ed667ab8103b93448dff7781a952e6c6df2bdc12
vladimirmiloradov <72631292+vladimirmiloradov@users.noreply.github.com>
2022-06-12 22:51:03 +0300

    Commit

---
+++ Exam/app/app.py
@@ -0,0 +1,36 @@
+import os
+from flask import Flask, render_template, send_file, abort, send_from_directory
+from sqlalchemy import MetaData
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+application = app
+
+app.config.from_pyfile('config.py')
+
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+metadata = MetaData(naming_convention=convention)
+db = SQLAlchemy(app, metadata=metadata)
+migrate = Migrate(app, db)
+
+from auth import bp as auth_bp, init_login_manager
+
+app.register_blueprint(auth_bp)
+
+
+init_login_manager(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
---
+++ Exam/app/models.py
@@ -0,0 +1,145 @@
+import sqlalchemy as sa
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import UserMixin
+from flask import url_for
+from app import db
+import os
+
+class Book(db.Model):
+    __tablename__ = 'books'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    short_desc = db.Column(db.Text, nullable=False)
+    publication_year = db.Column(db.Integer, nullable=False)
+    publishing_house = db.Column(db.String(100), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    volume = db.Column(db.String(100), nullable=False)
+    rating_sum = db.Column(db.Integer, nullable=False, default=0)
+    rating_num = db.Column(db.Integer, nullable=False, default=0)
+
+    def __repr__(self):
+        return '<Book %r>' % self.name
+    
+    @property
+    def rating(self):
+        if self.rating_num > 0:
+            return self.rating_sum / self.rating_num
+        return 0
+
+class Roles(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return '<Roles %r>' % self.name
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
+    first_name = db.Column(db.String(100), nullable=False)
+    middle_name = db.Column(db.String(100))
+    created_at = db.Column(db.DateTime, nullable=False, server_default=sa.sql.func.now())
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    @property
+    def full_name(self):
+        return ' '.join([self.last_name, self.first_name, self.middle_name or ''])
+
+    def __repr__(self):
+        return '<User %r>' % self.login
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    book_id = db.Column(db.Integer, db.ForeignKey('books.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    rating = db.Column(db.Integer, nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, server_default=sa.sql.func.now())
+
+    book = db.relationship('Book')
+    user = db.relationship('User')
+
+    def __repr__(self):
+        return '<Review %r>' % self.text
+
+class Image(db.Model):
+    __tablename__ = 'images'
+
+    id = db.Column(db.String(100), primary_key=True)
+    filename = db.Column(db.String(100), nullable=False)
+    mime_type = db.Column(db.String(100), nullable=False)
+    md5_hash = db.Column(db.String(100), nullable=False, unique=True)
+    book_id = db.Column(db.Integer, db.ForeignKey('books.id'))
+    object_id = db.Column(db.Integer)
+    object_type = db.Column(db.String(100))
+
+    book = db.relationship('Book')
+
+    def __repr__(self):
+        return '<Image %r>' % self.filename
+    
+    @property
+    def storage_filename(self):
+        _, ext = os.path.splitext(self.filename)
+        return self.id + ext
+    
+    @property
+    def url(self):
+        return url_for('image', image_id=self.id)
+
+class Genre(db.Model):
+    __tablename__ = 'genres'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<Genre %r>' % self.name
+
+class Join(db.Model):
+    __tablename__ = 'join'
+
+    id = db.Column(db.Integer, primary_key=True)
+    book_id = db.Column(db.Integer, db.ForeignKey('books.id'))
+    genre_id = db.Column(db.Integer, db.ForeignKey('genres.id'))
+
+    book = db.relationship('Book')
+    genre = db.relationship('Genre')
+
+    def __repr__(self):
+        return '<Join %r>' % self.id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
